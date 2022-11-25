51e5f43e7b7d5bbfec44d9e472db78a2830982a8
Priscila A <105573463+PriscahC@users.noreply.github.com>
2022-11-24 16:02:16 -0800

    prints binary numbers

--- print_binary.c
+++ print_binary.c
@@ -1,34 +1,16 @@
 #include "main.h"
 
 /**
- * printf_bin - prints a binary number.
- * @val: arguments.
- * Return: 1.
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed in binary
+ * @printed: hold the number of characters printed
  */
-int printf_bin(va_list val)
+void print_binary(unsigned int n, unsigned int *printed)
 {
-	int flag = 0;
-	int cont = 0;
-	int i, a = 1, b;
-	unsigned int num = va_arg(val, unsigned int);
-	unsigned int p;
-
-	for (i = 0; i < 32; i++)
+	if (n > 1)
 	{
-		p = ((a << (31 - i)) & num);
-		if (p >> (31 - i))
-			flag = 1;
-		if (flag)
-		{
-			b = p >> (31 - i);
-			_putchar(b + 48);
-			cont++;
-		}
+		*printed += 1;
+		print_binary(n >> 1, printed);
 	}
-	if (cont == 0)
-	{
-		cont++;
-		_putchar('0');
-	}
-	return (cont);
+	_putchar((n & 1) + '0');
 }
